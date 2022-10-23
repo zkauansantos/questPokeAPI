7c3053b689905b8fc6baacfa2c0241df37343c14
Kauan Santos <kauansantosdepontes@gmail.com>
2022-10-23 00:02:43 -0300

    pokemon info shown on screen, styling in progress

--- src/components/main/pokemon-info/index.js
+++ src/components/main/pokemon-info/index.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../../contexts/theme-context";
-import { SectionInfo, Div, DivImg, DivUl, Ul, Li, Paragraph } from "./styles";
+import { SectionInfo, Div, DivImg, DivDetails, Li, Ul } from "./styles";
 import {
   getPokemonsList,
   getPokemonsData,
@@ -12,49 +12,83 @@ const PokemonInfo = () => {
   const { theme } = useContext(ThemeContext);
   const { name } = useParams();
 
-  const [pokemon, setPokemon] = useState([{}]);
+  const [pokemonAbilities, setPokemonAbilities] = useState([]);
+  const [pokemonMoves, setPokemonMoves] = useState([]);
+  const [pokemonType, setPokemonType ] = useState([]);
+  const [pokemonImg, setPokemonImg] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-        const pokemonPromise =  await getPokemonsData(name)
-        console.log(pokemonPromise)
+        const pokemonData =  await getPokemonsData(name)
+        const pokemonAbilities = pokemonData.abilities.map((index) => getPokemonAbilities(index.ability.url))
+        const pokemonAbilityAndDescript = await Promise.all(pokemonAbilities)
 
+        setPokemonImg(pokemonData.sprites.other['official-artwork'].front_default)
+        setPokemonType([...pokemonData.types])
+        setPokemonMoves([...pokemonData.moves])
+        setPokemonAbilities([...pokemonAbilityAndDescript])
     }
 
     fetchData();
-
   }, [])
+
+  const PokemonType = () =>{
+    return(
+        <Li theme={theme}>
+          <h3>Type</h3>
+          {
+            pokemonType.map((index, position ) => {
+              return <p key={position}>{index.type.name}</p>
+            })
+          }
+        </Li>
+    )
+  }
+
+  const PokemonMoves = () => {
+    return(
+        <Li  theme={theme}>
+          <h3>Moves</h3>
+          <div style={{display: 'flex', flexWrap: 'wrap', textAlign: 'center'}}>
+            { pokemonMoves.map((index, position) => {
+              return (
+                  <p key={position}> {index.move.name},   </p>
+              )
+            })}
+          </div>
+
+        </Li>
+    )
+  }
+
+  const PokemonAbility = () => {
+    return (
+
+        <Li theme={theme}>
+          <h3>Abilities</h3>
+          {
+            pokemonAbilities.map((index, position) => {
+              return <p key={position}> {index.name}</p>
+            })
+          }
+       </Li>
+
+    )
+  }
 
   return (
     <SectionInfo theme={theme}>
       <Div>
-        <DivImg>Imagem</DivImg>
-        <DivUl>
-          {pokemon.map((index, position) => {
-            return (
-              <Ul key={position}  theme={theme}>
-                <Li theme={theme}>
-                  <h4>Type</h4>
-                  <Paragraph> DOLOR AMET</Paragraph>
-                </Li>
-                <Li theme={theme}>
-                  <h4>Moves</h4>
-                  <Paragraph>
-                    lOREM IPSUM DOLOR AMET LOREM IPSUM DOLOR AMET LOREM IPSUM
-                    DOLOR AMET LOREM IPSUM DOLOR AMET
-                  </Paragraph>
-                </Li>
-                <Li theme={theme}>
-                  <h4>Abilities</h4>
-                  <Paragraph>
-                    lOREM IPSUM DOLOR AMET LOREM IPSUM DOLOR AMET LOREM IPSUM
-                    DOLOR AMET LOREM IPSUM DOLOR AME
-                  </Paragraph>
-                </Li>
-              </Ul>
-            );
-          })}
-        </DivUl>
+        <DivImg theme={theme}>
+          <img style={{width: '80%'}} src={pokemonImg}></img>
+        </DivImg>
+        <DivDetails>
+          <Ul>
+            <PokemonType/>
+            <PokemonMoves/>
+            <PokemonAbility/>
+          </Ul>
+        </DivDetails>
       </Div>
     </SectionInfo>
   );
--- src/components/main/pokemon-info/styles.js
+++ src/components/main/pokemon-info/styles.js
@@ -4,16 +4,13 @@ import styled from "styled-components"
 const SectionInfo = styled.section`
     background-color: ${({theme}) => theme.backgroundMain };
     color: ${({theme}) => theme.color};
-    height: 100vh;
     display: flex;
     justify-content: center;
-
+    min-height: 80vh;
 `
 
 const Div = styled.div`
-display: grid;
-grid-template-rows: repeat(2, auto);
-justify-content: center;
+display: flex;
 flex-direction: column;
 align-items: center;
 color: ${({theme}) => theme.cardColor};
@@ -21,30 +18,42 @@ width: 90%;
 `
 
 const DivImg = styled.div`
+    height: 500px;
+    width: 500px;
     display: flex;
     justify-content: center;
-    height: 100%;
+    margin-top: 100px;
+    border-radius: 5%;
+    border: ${({theme}) => theme.borderCardAndButton};
+`
+const DivDetails = styled.div`
     margin-top: 55px;
-`
-const DivUl = styled.div`
-    height: 100%;
-`
-const Ul = styled.ul`
-    padding: 25px;
-    display: flex;
+    width: 100%;
     justify-content: space-around;
-    gap: 35px;
+`
+
+const Paragraph = styled.p`
+    margin-top: 15px;
+    font-weight: bold;
+    
 `
 const Li = styled.li`
     text-align: center;
     border: ${({theme}) => theme.borderCardAndButton};
     padding: 15px;
-    min-width: 200px;
+    :nth-child(2){
+        max-width: 600px;
+        flex-wrap: wrap;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+}
+`
+const Ul = styled.ul`
+    gap: 100px;
+    display: flex;
+    justify-content: center;
 
 `
-const Paragraph = styled.p`
-    margin-top: 15px;
-`
 
-
-export {SectionInfo, Div, Ul, DivImg, DivUl, Li, Paragraph}
+export {SectionInfo, Div, DivImg, DivDetails, Paragraph, Li, Ul}

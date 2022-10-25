45efb243900e06d860ed2b3d0932f4fd27681c63
zkauansantos <kauansantosdepontes@gmail.com>
2022-10-25 13:05:40 -0300

    organizing imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useContext } from 'react';
 import { ThemeProvider } from './contexts/theme-context';
 import { AppRoutes } from './pages/routes';
-import { ThemeContext } from './contexts/theme-context';
 import { GlobalStyle } from './global-style';
 
 function App() {
-
-  const {theme} =useContext(ThemeContext)
 
   return (
     <>

fccfa49a9d352a2984f20bc35c03b13b988c3076
zkauansantos <kauansantosdepontes@gmail.com>
2022-10-26 13:30:20 -0300

    error deploy netlify

--- src/components/main/select-filter-type/index.js
+++ src/components/main/select-filter-type/index.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext} from 'react'
 import { ThemeContext } from '../../../contexts/theme-context'
 import { getTypeData, getPokemonsData } from '../../../services/poke-api'
 import {DivFilter, Label, Select, Option} from './styles'

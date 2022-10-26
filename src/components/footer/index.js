bcb4de346d38f9315ca6c94b14f3c50cc60c1154
zkauansantos <kauansantosdepontes@gmail.com>
2022-10-26 13:26:11 -0300

    footer problems in deploy netlify

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,6 @@ import { ThemeContext } from "../../contexts/theme-context"
 import { useContext } from "react"
 import {FaLinkedin, FaGithub} from 'react-icons/fa'
 import {IoLogoWhatsapp} from 'react-icons/io'
-import { Link } from "react-router-dom"
 
 const Footer = () => {
 
@@ -14,9 +13,9 @@ const Footer = () => {
         <Div>
           <h3> Desenvolvido por ➡ Kauan Santos</h3>
           <List>
-            <li><a href='https://www.linkedin.com/in/kauansantosdepontes/' target='_blank' style={{color: theme.color}}><FaLinkedin/> Linkedin</a></li>
-            <li><a href='https://github.com/zkauansantos' target='_blank' style={{color: theme.color}}><FaGithub/> GitHub</a></li>
-            <li><a href= 'https://wa.me/5561982552480' target='_blank' style={{color: theme.color}}><IoLogoWhatsapp/> Contato</a></li>
+            <li><a rel="noreferrer" href='https://www.linkedin.com/in/kauansantosdepontes/' target='_blank' style={{color: theme.color}}><FaLinkedin/> Linkedin</a></li>
+            <li><a rel="noreferrer" href='https://github.com/zkauansantos' target='_blank' style={{color: theme.color}}><FaGithub/> GitHub</a></li>
+            <li><a rel="noreferrer" href= 'https://wa.me/5561982552480' target='_blank' style={{color: theme.color}}><IoLogoWhatsapp/> Contato</a></li>
           </List>
         </Div>
     </Container>

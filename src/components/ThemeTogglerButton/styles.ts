ca222eb868e771c7e81931ef1066ab47c1685e7b
Kauan Santos <84646178+zkauansantos@users.noreply.github.com>
2023-03-18 03:06:18 -0300

    fix error eslint

--- src/components/ThemeTogglerButton/styles.ts
+++ src/components/ThemeTogglerButton/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   position: absolute;
@@ -15,4 +15,4 @@ export const Container = styled.div`
       background: ${({theme}) => theme.background};
     }
   }
-`
+`;
